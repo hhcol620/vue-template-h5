5977f4800ffdceb47099315342d31c65cdfe3a10
hhcol <2838967868@qq.com>
2021-02-13 10:52:03 +0800

    处理移动端1px像素边框问题

--- src/packages/exampleD/views/test-A/index.vue
+++ src/packages/exampleD/views/test-A/index.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
         这是ED的ta
+        针对1px边框问题的处理
+        <div class="test"></div>
     </div>
 </template>
+
+
+<script>
+</script>
+
+<style lang="less" scoped>
+@import "@/styles/borderPX.less";
+// .borderPX;    (@position: border-bottom, @width: 1px, @color: #eaeaec)
+.test {
+    width: 200px;
+    height: 100px;
+    background-color: #fff;
+    .borderPX;
+}
+</style>
