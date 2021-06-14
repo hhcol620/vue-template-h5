ffc333d1ec21255b9ce0511be3f418fd21ad4f76
hhcol <2838967868@qq.com>
2021-06-14 16:16:51 +0800

    禁止缩放

--- src/main.js
+++ src/main.js
@@ -12,6 +12,8 @@ import 'animate.css';
 
 // 导入屏幕宽度处理js
 import '@/utils/rem.js';
+// 导入禁止缩放js
+import '@/utils/disableZoom.js';
 // 导入vant 组件库   按需导入
 import '@/components/vant-components.js';
 // 引入icon
